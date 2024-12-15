85db65bd0e2f15187d74177ccaabda8c0065e4e0
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-14 23:30:17 -0500

    Generar modulos y componentes con sus rutas

---
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-card',
+  templateUrl: './characters-card.component.html',
+  styleUrl: './characters-card.component.css'
+})
+export class CharactersCardComponent {
+
+}
---
+++ src/app/components/pages/characters/characters-details/characters-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-details',
+  templateUrl: './characters-details.component.html',
+  styleUrl: './characters-details.component.css'
+})
+export class CharactersDetailsComponent {
+
+}
