eb91fb9ce11972536263cf9cee42642db4ffbb62
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-15 13:04:32 -0500

    Agregar página de personajes destacados

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Character } from '@app/shared/interfaces/data.interfaces';
 
 @Component({
   selector: 'app-characters-card',
   templateUrl: './characters-card.component.html',
-  styleUrl: './characters-card.component.css'
+  styleUrl: './characters-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharactersCardComponent {
+  @Input() character: Character | null = null;
 
+  toggleFavorite(): void {}
 }
