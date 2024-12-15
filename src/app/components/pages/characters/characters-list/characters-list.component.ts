623e982427c5b0b74d9ec19937baedc34fef5b44
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-15 03:13:06 -0500

    Recuperar data desde GraphQL y guardar en observables

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '@app/shared/services/data.service';
 
 @Component({
   selector: 'app-characters-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './characters-list.component.css'
 })
 export class CharactersListComponent {
+    characters$ = this.dataSvc.characters$;
+    constructor(private dataSvc: DataService){}
 
+    ngOnInit(): void {
+    }
 }
